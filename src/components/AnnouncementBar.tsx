c8ce2430390e8aea1b8f4d8f7dbab7c1ff0beae6
Pradnyesh <your-github-email@example.com>
2026-06-12 15:46:03 +0530

    Remove Free Shipping announcement

--- src/components/AnnouncementBar.tsx
+++ src/components/AnnouncementBar.tsx
@@ -3,7 +3,6 @@ import './AnnouncementBar.css';
 const AnnouncementBar = () => {
   const announcements = [
     { text: 'Use Code ', highlight: 'VELOURA10', postText: ' for 10% OFF' },
-    { text: 'Free Shipping on Orders Over $75' },
     { text: '100% Hand-Poured Soy Wax' },
     { text: 'Explore our Signature Blends Collection' },
     { text: 'Contact Us for Custom Corporate Gifting' }
